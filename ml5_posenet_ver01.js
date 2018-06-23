66ca9f045fcbc5b09ea448362ff844d8e0fc8fc7
Vinay Bhajantri <vinmmb@gmail.com>
2018-06-23 21:13:51 +0200

    Adding abstract bezeir curves to the poses

--- ml5_posenet_ver01.js
+++ ml5_posenet_ver01.js
@@ -13,7 +13,6 @@ let poseNet;
 let poses = [];
 let skeletons = [];
 
-let tentacle;
 
 
 function setup() {
@@ -26,28 +25,18 @@ function setup() {
 
   // Hide the video element, and just show the canvas
   video.hide(); 
-
-  let point = new p5.Vector(300, 200);
-
-  let current = new Segment(point, 50, 0);
-  for (let i = 0; i < 20; i++) {
-    let next = new Segment(current, 10, i);
-    current.child = next;
-    current = next;
-  }
-  tentacle = current;
+  
 }
 
 function draw() {
   background(51);
-  //image(video, 0, 0, width, height);
+  //translate(width,0); // move to far corner
+  //scale(-1.0,1.0);    // flip x-axis backwards
+  image(video, 0, 0, width, height);
 
   // We can call both functions to draw all keypoints and the skeletons
   drawKeypoints();
   //drawSkeleton();
-
-  
-  
 }
 
 // A function to draw ellipses over the detected keypoints
@@ -55,26 +44,39 @@ function drawKeypoints()  {
   // Loop through all the poses detected
   for (let i = 0; i < poses.length; i++) {
     // For each pose detected, loop through all the keypoints
+
+    noFill();
+    stroke(255);
+    strokeWeight(10);
+
+    let shoulder_x = 0.5 * (poses[i].pose.keypoints[5].position.x + poses[i].pose.keypoints[6].position.x), 
+      shoulder_y = 0.5 * (poses[i].pose.keypoints[5].position.y + poses[i].pose.keypoints[6].position.y),
+      hip_x = 0.5 * (poses[i].pose.keypoints[11].position.x + poses[i].pose.keypoints[12].position.x),
+      hip_y = 0.5 * (poses[i].pose.keypoints[11].position.y + poses[i].pose.keypoints[12].position.y),
+      knee_x = 0.5 * (poses[i].pose.keypoints[13].position.x + poses[i].pose.keypoints[14].position.x), 
+      knee_y = 0.5 * (poses[i].pose.keypoints[13].position.y + poses[i].pose.keypoints[14].position.y),
+      ankle_x = 0.5 * (poses[i].pose.keypoints[15].position.x + poses[i].pose.keypoints[16].position.x),
+      ankle_y = 0.5 * (poses[i].pose.keypoints[15].position.y + poses[i].pose.keypoints[16].position.y),
+      leftArm_x = 0.5 * (poses[i].pose.keypoints[7].position.x + poses[i].pose.keypoints[9].position.x),
+      leftArm_y = 0.5 * (poses[i].pose.keypoints[7].position.y + poses[i].pose.keypoints[9].position.y),
+      rightArm_x = 0.5 * (poses[i].pose.keypoints[8].position.x + poses[i].pose.keypoints[10].position.x),
+      rightArm_y = 0.5 * (poses[i].pose.keypoints[8].position.y + poses[i].pose.keypoints[10].position.y);
+
+      bezier(shoulder_x, shoulder_y, hip_x, hip_y, knee_x, knee_y, ankle_x, ankle_y);
+      bezier(poses[i].pose.keypoints[5].position.x, poses[i].pose.keypoints[5].position.y, poses[i].pose.keypoints[7].position.x, poses[i].pose.keypoints[7].position.y, leftArm_x, leftArm_y, poses[i].pose.keypoints[9].position.x, poses[i].pose.keypoints[9].position.y);
+      bezier(poses[i].pose.keypoints[6].position.x, poses[i].pose.keypoints[6].position.y, poses[i].pose.keypoints[8].position.x, poses[i].pose.keypoints[8].position.y, rightArm_x, rightArm_y, poses[i].pose.keypoints[10].position.x, poses[i].pose.keypoints[10].position.y);
+    
+   
     for (let j = 0; j < poses[i].pose.keypoints.length; j++) {
       // A keypoint is an object describing a body part (j = 3 is the ear)
       let keypoint = poses[i].pose.keypoints[10];
       // Only draw an ellipse is the pose probability is bigger than 0.2
       if (keypoint.score > 0.2) {
-        fill(255, 255, 255, 100);
+
+        fill (255);
         noStroke();
         ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
 
-        tentacle.follow(keypoint.position.x, keypoint.position.y);
-        tentacle.update();
-        tentacle.show();
-
-        let next = tentacle.par;
-        while (next) {
-          next.follow();
-          next.update();
-          next.show();
-          next = next.par;
-          }
       }
     }
   }
